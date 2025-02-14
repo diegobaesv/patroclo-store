a847636ceb4194d3467d99457797728a95a484c9
DiegoB <diegobaesv@gmail.com>
2025-02-13 22:33:39 -0500

    Create Categoria.php

---
+++ app/models/Categoria.php
@@ -0,0 +1,29 @@
+<?php
+class Categoria {
+    private $conn;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function listarCategorias(){
+        
+    }
+
+    public function obtenerCategoria(){
+    
+    }
+
+    public function insertarCategoria(){
+
+    }
+
+    public function actualizarCategoria(){
+
+    }
+
+    public function darBajaCategoria(){
+
+    }
+
+}
