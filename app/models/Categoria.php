75ff6684cb8bec24f3e4c6654842c1dbc2cc1f3c
DiegoB <diegobaesv@gmail.com>
2025-02-18 20:18:58 -0500

    select actualizado

--- app/models/Categoria.php
+++ app/models/Categoria.php
@@ -1,13 +1,18 @@
 <?php
 class Categoria {
     private $conn;
+    private $tableName = "categorias";
 
     public function __construct($db){
         $this->conn = $db;
     }
 
     public function listarCategorias(){
-        
+        $query = "SELECT * FROM " . $this->tableName . " WHERE estado_auditoria = '1'";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
 
     public function obtenerCategoria(){
--- public/index.php
+++ public/index.php
@@ -1,14 +1,24 @@
 <?php
     require_once __DIR__ . '/../app/config/Database.php';
+    require_once __DIR__ . '/../app/models/Categoria.php';
+
+    echo "<p>Hola desde index.php</p>";
 
     $database = new Database();
     $conn = $database->getConnection();
 
     if($conn) {
-        echo "La conexion ha sido exitosa :D";
+        echo "<p>La conexion ha sido exitosa :D</p>";
     } else {
-        echo "La conexion ha fallado :(";
+        echo "<p>La conexion ha fallado :(</p>";
     }
 
-    echo "Hola desde index.php";
+    $categoriaModel = new Categoria($conn);
+
+    $categorias = $categoriaModel->listarCategorias();
+
+    echo "<div>";
+    print_r($categorias);
+    echo "</div>"
+
 ?>
