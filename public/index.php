22bf5404e6883033d7bf7cb11d08655b77f04f18
DiegoB <diegobaesv@gmail.com>
2025-02-18 20:40:32 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,24 +1,22 @@
 <?php
-    require_once __DIR__ . '/../app/config/Database.php';
-    require_once __DIR__ . '/../app/models/Categoria.php';
+require_once __DIR__ . '/../app/config/Database.php';
+require_once __DIR__ . '/../app/models/Categoria.php';
 
-    echo "<p>Hola desde index.php</p>";
+echo "<p>Hola desde index.php</p>";
 
-    $database = new Database();
-    $conn = $database->getConnection();
+$database = new Database();
+$conn = $database->getConnection();
 
-    if($conn) {
-        echo "<p>La conexion ha sido exitosa :D</p>";
-    } else {
-        echo "<p>La conexion ha fallado :(</p>";
-    }
+if ($conn) {
+    echo "<p>La conexion ha sido exitosa :D</p>";
+} else {
+    echo "<p>La conexion ha fallado :(</p>";
+}
 
-    $categoriaModel = new Categoria($conn);
+$categoriaModel = new Categoria($conn);
 
-    $categorias = $categoriaModel->listarCategorias();
+$categorias = $categoriaModel->listarCategorias();
 
-    echo "<div>";
-    print_r($categorias);
-    echo "</div>"
-
-?>
+echo "<div>";
+print_r($categorias);
+echo "</div>";
